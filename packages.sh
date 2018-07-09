9775da2b0df5484c81c145073e6ab9c69d9e20d4
markieeee <marcus.d.ellison@gmail.com>
2018-07-09 18:53:55 -0500

    forgot to compile lol

--- packages.sh
+++ packages.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+dpkg-deb -bZgzip projects/AEVUS debs
+dpkg-deb -bZgzip projects/animate debs
+dpkg-deb -bZgzip projects/BioProtect debs
+dpkg-deb -bZgzip projects/CarBridge debs
+dpkg-deb -bZgzip projects/CCMusicArtwork debs
+dpkg-deb -bZgzip projects/Clotus debs
+dpkg-deb -bZgzip projects/cocoatop debs
+dpkg-deb -bZgzip projects/Cylinder debs
 dpkg-deb -bZgzip projects/inApp debs
+dpkg-deb -bZgzip projects/Mage debs
+dpkg-deb -bZgzip projects/MageFix debs
+dpkg-deb -bZgzip projects/NoTrackpadDelay debs
+dpkg-deb -bZgzip projects/powerdown debs
+dpkg-deb -bZgzip projects/TapTapFlip debs
